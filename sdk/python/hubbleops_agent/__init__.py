085ec5e39a30cd2a96fa223a581b5fa779395059
Jaya Krishna J <jastijayakrishna47@gmail.com>
2026-07-02 19:14:56 +0530

    refactor: remove legacy proxy, loop detector, sdk, and demo corpus code

--- sdk/python/hubbleops_agent/__init__.py
+++
@@ -1,3 +0,0 @@
-from .langchain import HubbleOpsClient, HubbleOpsLoopBlocked, action, wrap_action, wrap_tool
-
-__all__ = ["HubbleOpsClient", "HubbleOpsLoopBlocked", "action", "wrap_action", "wrap_tool"]
